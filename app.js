d4a6113a75da7d3c7d5735fd24d56707236d5487
pnichols789 <pnichols789@gmail.com>
2022-09-14 14:12:25 -0500

    -

---
+++ app.js
@@ -0,0 +1,5 @@
+// functionality needed:
+// plus and minus buttons adjust the quantity of each item
+// adjusting quantity changes the value for product total, subtotal, tax, and total
+// remove button pop up verification and then removes item
+// Optional: discount percentage, total savings

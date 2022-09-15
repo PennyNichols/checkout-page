703eed5124ff2b06c0926b46f240c7ff553f5493
pnichols789 <pnichols789@gmail.com>
2022-09-15 10:02:31 -0500

    -

--- app.js
+++ app.js
@@ -4,20 +4,31 @@
 // remove button pop up verification and then removes item
 // Optional extras: discount percentage, total savings
 
-
-const tax = 0.18;
+const tax = 18;
 const shipping = 15;
 
-let products = document.querySelector('.products');
-products.addEventListener('click', (e) =>{
-    if (e.target.classList.contains('remove-product')){
-        console.log('working');
-        if (confirm(`Are you sure you want to remove this item?`)) {
+let products = document.querySelector(".products");
+products.addEventListener("click", (e) => {
+	if (e.target.classList.contains("remove-product")) {
+		if (confirm(`Are you sure you want to remove this item?`)) {
+			e.target.parentElement.parentElement.parentElement.remove();
+		}
+	} else if (e.target.classList.contains("plus")) {
+		e.target.previousElementSibling.innerText++;
+	} else if (e.target.classList.contains("minus")) {
+        if (e.target.nextElementSibling.innerText > 1) {
+            e.target.nextElementSibling.innerText--;
+		} else if (confirm(`Are you sure you want to remove this item?`)) {
             e.target.parentElement.parentElement.parentElement.remove();
-        }
-    } else if (e.target.classList.contains('plus')){
-        console.log('working');
-    }
-})
-
-
+		}
+	}
+    
+const calcLine = (productDetails) => {
+    console.log(productDetails);
+    let qty = productDetails.querySelector('#product-quantity').innerText;
+    let reducedPrice = productDetails.querySelector('strong').innerText;
+    let originalPrice = productDetails.querySelector('.line-through').innerText;
+    
+        
+}
+});
